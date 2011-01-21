d6f28ffb1f9038f3bc31f445547d6634597aa5b6
Tim Dysinger <tim@dysinger.net>
2011-01-20 21:18:08 -1000

    Refine test a little

--- test/hackamore/test.clj
+++ test/hackamore/test.clj
@@ -6,14 +6,14 @@
   (testing "with a context"
     (let [ctx (context)]
       (testing "we should be able to define a simple route"
-        (route ctx (from "vm:in") (to "vm:out"))
+        (doto ctx
+          (route (from "vm:a") (to "vm:b"))
+          (start))
         (testing "and the messages should flow correctly"
-          (start ctx)
-          ;; send message
           (-> ctx
               (.createProducerTemplate)
-              (.sendBody "vm:in" "ohai"))
+              (.sendBody "vm:a" "ohai"))
           (is (= "ohai" (-> ctx
                             (.createConsumerTemplate)
-                            (.receiveBody "vm:out"))))
-          (stop ctx))))))
+                            (.receiveBody "vm:b")))))
+        (stop ctx)))))
