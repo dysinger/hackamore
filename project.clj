4838132b806d39922710adc9385a49bca8b9c178
Tim Dysinger <tim@dysinger.net>
2011-01-20 21:12:01 -1000

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject hackamore "0.1.0-SNAPSHOT"
+  :description "Hackamore: Clojure Library for Apache Camel"
+  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
+                 [org.apache.camel/camel-core "2.5.0"]])
